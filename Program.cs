7cd2a43d36a26f63147425add00693eb45a4dc81
Joe <accounts+github@harmon.team>
2022-12-06 22:49:26 -0500

    First commit to submit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using BRDF_energy_tester;
+using System;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 static string TestMethod()
 {
@@ -8,3 +11,40 @@ static string TestMethod()
 }
 
 Console.WriteLine(TestMethod());
+
+
+
+public class TestClass : TestInterface
+{
+    private string m_child = "Ella";
+    private string m_parent = "Joe";
+
+    public void TestCLass(string name)
+    {
+        Name = name;
+    }
+
+    public string? Name { get; set; }
+    public string Child
+    {
+        get { return m_child; }
+        set { m_child = value; }
+    }
+
+    public string Parent
+    {
+        get { return m_parent; }
+        set { m_parent = value; }
+    }
+
+    public string GetParent()
+    {
+        return m_parent;
+    }
+
+}
+
+
+
+
+
