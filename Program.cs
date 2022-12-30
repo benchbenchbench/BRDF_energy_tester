28eb5f827ea9c61f47cc9acee2b3b9c9a3cfbe84
Joe <accounts+github@harmon.team>
2022-12-29 22:58:04 -0500

    Adding spherical geometry class to hold static formulae. Adding code to sum up the surface area of the sphere by chunks.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,36 @@ using System.Security.Cryptography.X509Certificates;
 
 static string TestMethod()
 {
-    return "This is a test method.";
+    double halfSphereTest = BRDF_energy_tester.SphericalGeometry.SurfaceAreaPatch( Math.PI, 0.0d, Math.PI, 0.0d);
+    double quarterSphereTest = BRDF_energy_tester.SphericalGeometry.SurfaceAreaPatch(Math.PI, 0.0d, (Math.PI/2.0d), 0.0d);
+    double fullSphereTest = SphericalGeometry.SurfaceArea(1.0d);
+
+
+    double numberOfSegments_long = 4;
+    double segment_long = (Math.PI * 2.0d) / numberOfSegments_long;
+
+    double numberOfSegments_lat = 4;
+    double segment_lat = (Math.PI / 2.0d) / numberOfSegments_lat;
+
+    double totalArea = 0.0d;
+    for (int i = 0; i < numberOfSegments_long; i++)
+    {
+        
+        double phi0 = (double)i * segment_long;
+        double phi1 = (double)(i + 1) * segment_long;
+        for(int j = 0; j < numberOfSegments_lat; j++)
+        {
+            double theta0 = (double)j * segment_lat;
+            double theta1 = (double)(j + 1) * segment_lat;
+            totalArea += Math.Abs(SphericalGeometry.SurfaceAreaPatch(theta1, theta0, phi1, phi0));
+        }
+        
+    }
+    
+    
+    
+    
+    return string.Format("Result: {0}", totalArea);
 }
 
 Console.WriteLine(TestMethod());
---
+++ SphericalGeometry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRDF_energy_tester
+{
+    public static class SphericalGeometry
+    {
+
+        /// <summary>
+        /// Calculates the surface area patch of a sphere by the spherical coordinates theta and phi. The patch is rectangular. 
+        /// Theta and phi are not great circles but the lattitude and longitude spherical coordinates
+        /// </summary>
+        /// <param name="theta1">The starting position of the "lattitude" marker. Unit should be provided in radians.</param>
+        /// <param name="theta2">The ending position of the "lattitude" marker. Unit should be provided in radians.</param>
+        /// <param name="phi1">The starting position of the "longitude" marker. Unit should be provided in radians.</param>
+        /// <param name="phi2">the ending position of the "longitude" marker. Unit should be provided in radians.</param>
+        /// <param name="R">The radius of the sphere. Assumed to be 1.0 if no value given.</param>
+        /// <returns>The surface area in units squared. </returns>
+        public static double SurfaceAreaPatch(double theta1, double theta2, double phi1, double phi2, double R = 1.0d)
+        {
+            return (theta2 - theta1) * (Math.Cos(phi1) - Math.Cos(phi2)) * R * R;
+        }
+
+        /// <summary>
+        /// Calculates the surface area of a sphere based upon the radius length. 
+        /// </summary>
+        /// <param name="radius">Size of the sphere. Assumed to be 1.0 if no value given.</param>
+        /// <returns></returns>
+        public static double SurfaceArea(double radius = 1.0d)
+        {
+            return 4.0d * Math.PI * radius * radius;
+        }
+    }
+}
